6a8a2cb4085bb47547e318ba9b2aa71839b0be75
hazem_said <haazemsaidd@gmail.com>
2024-06-27 17:22:44 +0200

    finish the home page {using the futurebuilder to get data for api }

--- lib/model/Customcard.dart
+++ lib/model/Customcard.dart
@@ -1,57 +1,83 @@
+import 'package:appstore/model/product.dart';
 import 'package:awesome_icons/awesome_icons.dart';
 import 'package:flutter/material.dart';
 
 class customecard extends StatelessWidget {
-  const customecard({super.key});
+  final product itme;
+  customecard({super.key, required this.itme});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 130,
-      width: 200,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(5.0),
-        child: Card(
-            color: Colors.white,
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 10.0,
-                vertical: 10.0,
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          height: 200,
+          width: 300,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(.6),
+                spreadRadius: 0,
+                blurRadius: 20,
+                offset: Offset(5, 20),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    "data",
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.grey,
-                    ),
+            ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Card(
+                color: Colors.white,
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                    vertical: 10.0,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       Text(
-                        r"$2000",
-                        style: TextStyle(fontSize: 20),
+                        "${itme.title.toString().substring(0, 6)}",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
                       ),
-                      Icon(
-                        FontAwesomeIcons.solidHeart,
-                        color: Colors.red,
-                        size: 20.0,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "${itme.price}" r' $',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Icon(
+                            FontAwesomeIcons.solidHeart,
+                            color: Colors.red,
+                            size: 20.0,
+                          )
+                        ],
                       )
                     ],
-                  )
-                ],
-              ),
-            )),
-      ),
+                  ),
+                )),
+          ),
+        ),
+        Positioned(
+          top: 20,
+          right: 10,
+          left: 30,
+          // left: 10,
+          child: Image.network(
+            height: 90,
+            width: 90,
+            "${itme.image}",
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,14 @@
 import 'package:appstore/model/product.dart';
-import 'package:appstore/model/product.dart';
 import 'package:awesome_icons/awesome_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import '../model/product.dart';
 import '/services/get_products_api.dart';
 import '/model/Customcard.dart';
-import '/model/Customcard.dart';
 
 class Home_Page extends StatelessWidget {
   // Home_Page({super.key});
+  Future<List<product>> items = get_products_api().get_products();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,16 +16,78 @@ class Home_Page extends StatelessWidget {
           backgroundColor: Colors.white,
           elevation: 0,
           title: Center(
-            child: Text('New items'),
-          ),
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('New items'),
+              SizedBox(
+                width: 10,
+              ),
+              Icon(FontAwesomeIcons.cartPlus)
+            ],
+          )),
         ),
-        body: GridView.builder(
-            itemCount: 56,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-            ),
-            itemBuilder: (context, indx) {
-              return customecard();
-            }));
+        body: FutureBuilder<List<product>>(
+          future: items,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (snapshot.hasData) {
+              // Assuming you want to display the products in a list
+              return Column(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: SizedBox(
+                      height: 100,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 10000,
+                    child: GridView.builder(
+                        itemCount: snapshot.data!.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 10,
+                          crossAxisSpacing: 10,
+                        ),
+                        itemBuilder: (context, index) {
+                          product item = snapshot.data![index];
+                          return customecard(itme: item);
+                        }),
+                  ),
+                ],
+              );
+            } else {
+              return Center(child: Text('No products found'));
+            }
+          },
+        ));
   }
 }
+/*Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+          Expanded(
+            flex: 99,
+            child: GridView.builder(
+                itemCount: 60,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 9 / 6.3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemBuilder: (context, index) {
+                  return customecard();
+                }),
+          ),
+        ],
+      ),*/
